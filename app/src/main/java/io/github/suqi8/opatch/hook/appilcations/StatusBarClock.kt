337724d34175db43e675557e8c085f4779626991
suqi8 <3383787570@qq.com>
2024-12-25 15:43:20 +0800

    feat(ui): 添加关于页面和设置页面
    - 新增 about_group 和 about_setting 页面
    - 更新 Fun_android_package_manager_services 和其他页面的样式
    - 添加 GitHub 和 QQ群图标
    - 更新 Kotlin 和 Compose 依赖版本

--- app/src/main/java/io/github/suqi8/opatch/hook/appilcations/StatusBarClock.kt
+++ app/src/main/java/io/github/suqi8/opatch/hook/appilcations/StatusBarClock.kt
@@ -77,6 +77,7 @@ class StatusBarClock : YukiBaseHooker() {
                                     )
                                 }
                             }
+
                         }
 
                         if (updateSpeed != 0) {
