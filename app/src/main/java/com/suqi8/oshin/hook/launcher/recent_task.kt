aa3b1c5aa05f398e4ebdfb441a4223228bdd959e
suqi8 <3383787570@qq.com>
2025-03-11 22:59:07 +0800

    feat(app): 优化最近任务页面内存信息显示
    -   优化了最近任务页面内存信息显示：
        -   修改 `isAllowMemoryInfoDisplay` 的 Hook 逻辑，使其始终返回 `true`，以允许显示内存信息。
        -   修改 `needMemoryDetail` 的 Hook 逻辑，使其始终返回 `true`，以显示内存详细信息。
        - 修改Hook 的方式 为 `before` 以提前更改值。
    
    Signed-off-by: suqi8 <3383787570@qq.com>

--- app/src/main/java/com/suqi8/oshin/hook/launcher/recent_task.kt
+++ app/src/main/java/com/suqi8/oshin/hook/launcher/recent_task.kt
@@ -3,6 +3,7 @@ package com.suqi8.oshin.hook.launcher
 import android.annotation.SuppressLint
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.type.java.BooleanType
 
 class recent_task: YukiBaseHooker() {
     @SuppressLint("UseCompatLoadingForDrawables", "SetTextI18n")
@@ -12,18 +13,22 @@ class recent_task: YukiBaseHooker() {
                 "com.oplus.quickstep.memory.MemoryInfoManager".toClass().apply {
                     method {
                         name = "isAllowMemoryInfoDisplay"
+                        emptyParam()
+                        returnType = BooleanType
                     }.hook {
-                        after {
-                            args[0] = true
+                        before {
+                            result = true
                         }
                     }
                 }
                 "com.oplus.quickstep.memory.MemoryInfoManager".toClass().apply {
                     method {
                         name = "needMemoryDetail"
-                    }.hook{
-                        after {
-                            args[0] = true
+                        emptyParam()
+                        returnType = BooleanType
+                    }.hook {
+                        before {
+                            result = true
                         }
                     }
                 }
