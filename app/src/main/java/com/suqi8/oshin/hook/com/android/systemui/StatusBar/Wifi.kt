8021838077082f10f4dc56bd62c359df504eed9f
suqi8 <3383787570@qq.com>
2025-03-28 12:01:29 +0800

    feat(app): 优化网速指示器
    - 新增并优化了网速指示器的配置功能：
        - 新增`隐藏b/s`，`隐藏空格`，`交换上下行位置`以及`网速指示器前置`选项，允许用户对网速指示器的显示格式进行精细化调整。
        - 优化`图标指示器`功能，支持将图标指示器放置于文本之前。
    -  调整了网速指示器的样式：
        - 调整了样式为`网速双行显示`时，上下行指示器箭头的位置逻辑，现在支持前置或后置。
        - 当用户开启`交换上下行位置`后, 会交换上下行数据和上下行图标的位置
        - 新增`隐藏空格`功能, 会隐藏网速数字以及单位之间的空格.
        - 新增`隐藏b/s`功能, 会隐藏网速末尾的b/s单位.
    - 修复了双行显示时的相关问题:
        - 现在支持对双行显示的上下行字体单独设置字体大小。
        - 修复了当切换隐藏方式后，布局没有重新计算导致布局错乱的问题.
    -   优化了逻辑:
        - 优化了上下行速度格式化的代码逻辑, 现在代码更加清晰.
        - 优化了图标指示器的实现, 支持通过配置修改图标指示器的位置.
        - 现在在`NetworkSpeedView`加载之前判断网速是否启用.
    -   更新了`strings.xml`：
        - 添加了`隐藏b/s`，`隐藏空格`，`交换上下行位置`，`网速指示器前置`相关的字符串资源.
    
    Signed-off-by: suqi8 <3383787570@qq.com>

--- app/src/main/java/com/suqi8/oshin/hook/com/android/systemui/StatusBar/Wifi.kt
+++ app/src/main/java/com/suqi8/oshin/hook/com/android/systemui/StatusBar/Wifi.kt
@@ -4,12 +4,14 @@ import android.annotation.SuppressLint
 import android.net.TrafficStats
 import android.os.SystemClock
 import android.util.TypedValue
+import android.view.View
 import android.widget.FrameLayout
 import android.widget.GridLayout.LayoutParams
 import android.widget.TextView
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.field
 import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.log.YLog
 import java.time.Instant
 
 class Wifi: YukiBaseHooker() {
@@ -18,9 +20,19 @@ class Wifi: YukiBaseHooker() {
     val hide_on_slow = prefs("systemui\\status_bar_wifi").getBoolean("hide_on_slow", false)
     val hide_when_both_slow = prefs("systemui\\status_bar_wifi").getBoolean("hide_when_both_slow", false)
     val icon_indicator = prefs("systemui\\status_bar_wifi").getInt("icon_indicator",0)
+    val hide_bs = prefs("systemui\\status_bar_wifi").getBoolean("hide_bs", false)
+    val hide_space = prefs("systemui\\status_bar_wifi").getBoolean("hide_space", false)
+    val swap_upload_download = prefs("systemui\\status_bar_wifi").getBoolean("swap_upload_download", false)
+    val upload_font_size = prefs("systemui\\status_bar_wifi").getInt("upload_font_size",-1)
+    val download_font_size = prefs("systemui\\status_bar_wifi").getInt("download_font_size",-1)
+    val position_speed_indicator_front = prefs("systemui\\status_bar_wifi").getBoolean("position_speed_indicator_front", false)
+    val speed_font_size = prefs("systemui\\status_bar_wifi").getInt("speed_font_size",-1)
+    val unit_font_size = prefs("systemui\\status_bar_wifi").getInt("unit_font_size",-1)
+
 
     @SuppressLint("SetTextI18n")
     override fun onHook() {
+        if (!prefs("systemui\\status_bar_wifi").getBoolean("status_bar_wifi", false)) return
         loadApp("com.android.systemui"){
             "com.oplus.systemui.statusbar.phone.netspeed.widget.NetworkSpeedView".toClass().apply {
                 method {
@@ -28,39 +40,50 @@ class Wifi: YukiBaseHooker() {
                 }.hook {
                     before {
                         instance<FrameLayout>().layoutParams.takeIf { it != null }?.width = LayoutParams.WRAP_CONTENT
+
+
                         if (StyleSelectedOption == 0) {
+                            val TotalByte = getCurrentSpeed()
+                            val isslow_speed = if (hide_on_slow) (((TotalByte.first / 1024) + (TotalByte.second / 1024)) <= slow_speed_threshold) else false
+                            YLog.info(isslow_speed.toString())
                             field {
                                 name = "mSpeedNumber"
                                 type = "android.widget.TextView"
                             }.get(instance).cast<TextView>()!!.apply {
-                                if (prefs("systemui\\status_bar_wifi").getInt("speed_font_size",-1) != -1) setTextSize(TypedValue.COMPLEX_UNIT_DIP, prefs("systemui\\status_bar_wifi").getInt("speed_font_size",-1).toFloat())
+                                if (speed_font_size != -1) setTextSize(TypedValue.COMPLEX_UNIT_DIP, speed_font_size.toFloat())
                                 //layoutParams.height = LayoutParams.WRAP_CONTENT
                                 layoutParams.width = LayoutParams.WRAP_CONTENT
+                                if (isslow_speed) visibility = View.GONE else visibility = View.VISIBLE
                             }
                             field {
                                 name = "mSpeedUnit"
                                 type = "android.widget.TextView"
                             }.get(instance).cast<TextView>()!!.apply {
-                                if (prefs("systemui\\status_bar_wifi").getInt("unit_font_size",-1) != -1)setTextSize(TypedValue.COMPLEX_UNIT_DIP, prefs("systemui\\status_bar_wifi").getInt("unit_font_size",-1).toFloat())
+                                if (unit_font_size != -1) setTextSize(TypedValue.COMPLEX_UNIT_DIP, unit_font_size.toFloat())
                                 //layoutParams.height = LayoutParams.WRAP_CONTENT
                                 layoutParams.width = LayoutParams.WRAP_CONTENT
+                                if (isslow_speed) visibility = View.GONE else visibility = View.VISIBLE
                             }
                         }
+
+
+
                         if (StyleSelectedOption == 1) {
                             val TotalByte = getCurrentSpeed()
                             field {
                                 name = "mSpeedNumber"
                                 type = "android.widget.TextView"
                             }.get(instance).cast<TextView>()!!.apply {
-                                setTextSize(TypedValue.COMPLEX_UNIT_DIP, if (prefs("systemui\\status_bar_wifi").getInt("upload_font_size",-1) != -1) prefs("systemui\\status_bar_wifi").getInt("upload_font_size",-1).toFloat() else 8F)
+                                setTextSize(TypedValue.COMPLEX_UNIT_DIP,
+                                    if (!swap_upload_download) {
+                                        if (upload_font_size != -1) upload_font_size.toFloat() else 8F
+                                    } else {
+                                        if (download_font_size != -1) download_font_size.toFloat() else 8F
+                                    }
+                                )
                                 //layoutParams.height = LayoutParams.WRAP_CONTENT
                                 layoutParams.width = LayoutParams.WRAP_CONTENT
-                                var uptext = formatBytes(TotalByte.second)
-                                uptext = uptext + txArrow(icon_indicator, TotalByte.second/1024, slow_speed_threshold)
-                                if (hide_when_both_slow) {
-                                    if ((TotalByte.second / 1024 <= slow_speed_threshold) && (TotalByte.first / 1024 <= slow_speed_threshold)) uptext = ""
-                                }
-                                if (hide_on_slow && !hide_when_both_slow) if (TotalByte.first / 1024 <= slow_speed_threshold) uptext = ""
+                                val uptext = if (!swap_upload_download) txSpeed(TotalByte) else rxSpeed(TotalByte)
                                 text = uptext
                             }
                             val mSpeedUnit = field {
@@ -68,30 +91,55 @@ class Wifi: YukiBaseHooker() {
                                 type = "android.widget.TextView"
                             }.get(instance).cast<TextView>()
                             mSpeedUnit!!.apply {
-                                setTextSize(TypedValue.COMPLEX_UNIT_DIP, if (prefs("systemui\\status_bar_wifi").getInt("download_font_size",-1) != -1) prefs("systemui\\status_bar_wifi").getInt("download_font_size",-1).toFloat() else 8F)
+                                setTextSize(TypedValue.COMPLEX_UNIT_DIP,
+                                    if (!swap_upload_download) {
+                                        if (download_font_size != -1) download_font_size.toFloat() else 8F
+                                    } else {
+                                        if (upload_font_size != -1) upload_font_size.toFloat() else 8F
+                                    })
                                 //layoutParams.height = LayoutParams.WRAP_CONTENT
                                 layoutParams.width = LayoutParams.WRAP_CONTENT
-                                var uptext = formatBytes(TotalByte.first)
-                                uptext = uptext + rxArrow(icon_indicator, TotalByte.first/1024, slow_speed_threshold)
-                                if (hide_when_both_slow) {
-                                    if ((TotalByte.second / 1024 <= slow_speed_threshold) && (TotalByte.first / 1024 <= slow_speed_threshold)) uptext = ""
-                                }
-                                if (hide_on_slow && !hide_when_both_slow) if (TotalByte.second / 1024 <= slow_speed_threshold) uptext = ""
+
+                                val uptext = if (!swap_upload_download) rxSpeed(TotalByte) else txSpeed(TotalByte)
                                 text = uptext
                             }
                         }
-                        resultNull()
+                        if (StyleSelectedOption != 0) resultNull()
                     }
                 }
             }
         }
     }
+
+    fun txSpeed(TotalByte: Pair<Long, Long>): String {
+        var uptext = formatBytes(TotalByte.second)
+        val txArrow = txArrow(icon_indicator, TotalByte.second/1024, slow_speed_threshold)
+        uptext = if (!position_speed_indicator_front) uptext + txArrow else txArrow + uptext
+        if (hide_when_both_slow) {
+            if ((TotalByte.second / 1024 <= slow_speed_threshold) && (TotalByte.first / 1024 <= slow_speed_threshold)) uptext = ""
+        }
+        if (hide_on_slow && !hide_when_both_slow) if (TotalByte.first / 1024 <= slow_speed_threshold) uptext = ""
+        return uptext
+    }
+
+    fun rxSpeed(TotalByte: Pair<Long, Long>): String {
+        var uptext = formatBytes(TotalByte.first)
+        val rxArrow = rxArrow(icon_indicator, TotalByte.first/1024, slow_speed_threshold)
+        uptext = if (!position_speed_indicator_front) uptext + rxArrow else rxArrow + uptext
+        if (hide_when_both_slow) {
+            if ((TotalByte.second / 1024 <= slow_speed_threshold) && (TotalByte.first / 1024 <= slow_speed_threshold)) uptext = ""
+        }
+        if (hide_on_slow && !hide_when_both_slow) if (TotalByte.second / 1024 <= slow_speed_threshold) uptext = ""
+        return uptext
+    }
     
     fun txArrow(icons: Int, Speed: Long, lowLevel: Int): String {
+        val isfront2 = if (Speed < lowLevel) if (position_speed_indicator_front) "▵ " else " ▵" else if (position_speed_indicator_front) "▴ " else " ▴"
+        val isfront3 = if (Speed < lowLevel) if (position_speed_indicator_front) "☖ " else " ☖" else if (position_speed_indicator_front) "☗ " else " ☗"
         return when (icons) {
             1 -> if (Speed < lowLevel) "△" else "▲"
-            2 -> if (Speed < lowLevel) " ▵" else " ▴"
-            3 -> if (Speed < lowLevel) " ☖" else " ☗"
+            2 -> isfront2
+            3 -> isfront3
             4 -> if (Speed < lowLevel) "↑" else "↑"
             5 -> if (Speed < lowLevel) "⇧" else "⇧"
             else -> ""
@@ -99,10 +147,12 @@ class Wifi: YukiBaseHooker() {
     }
 
     fun rxArrow(icons: Int, Speed: Long, lowLevel: Int): String {
+        val isfront2 = if (Speed < lowLevel) if (position_speed_indicator_front) "▿ " else " ▿" else if (position_speed_indicator_front) "▾ " else " ▾"
+        val isfront3 = if (Speed < lowLevel) if (position_speed_indicator_front) "⛉ " else " ⛉" else if (position_speed_indicator_front) "⛊ " else " ⛊"
         return when (icons) {
             1 -> if (Speed < lowLevel) "▽" else "▼"
-            2 -> if (Speed < lowLevel) " ▿" else " ▾"
-            3 -> if (Speed < lowLevel) " ⛉" else " ⛊"
+            2 -> isfront2
+            3 -> isfront3
             4 -> if (Speed < lowLevel) "↓" else "↓"
             5 -> if (Speed < lowLevel) "⇩" else "⇩"
             else -> ""
@@ -146,9 +196,9 @@ class Wifi: YukiBaseHooker() {
     }
 
     fun formatBytes(bytes: Long): String {
-        if (bytes <= 0) return "0 B"
+        if (bytes <= 0) return "0 b/s"
 
-        val units = arrayOf("B", "KB", "MB", "GB", "TB")
+        val units = arrayOf("", "K", "M", "G", "T")
         var size = bytes.toDouble()
         var unitIndex = 0
 
@@ -156,12 +206,14 @@ class Wifi: YukiBaseHooker() {
             size /= 1024.0
             unitIndex++
         }
+        val bs = "b/s"
+        val space = if (hide_space) "" else " "
 
         return when {
-            unitIndex == 0 -> "${size.toLong()} ${units[unitIndex]}"        // 字节不保留小数
-            size < 10 -> "%.2f ${units[unitIndex]}".format(size)            // 小于10保留两位小数
-            size < 100 -> "%.1f ${units[unitIndex]}".format(size)           // 小于100保留一位小数
-            else -> "%.0f ${units[unitIndex]}".format(size)                  // 整数显示
-        }
+            unitIndex == 0 -> "${size.toLong()}${space}${units[unitIndex]}"        // 字节不保留小数
+            size < 10 -> "%.2f${space}${units[unitIndex]}".format(size)            // 小于10保留两位小数
+            size < 100 -> "%.1f${space}${units[unitIndex]}".format(size)           // 小于100保留一位小数
+            else -> "%.0f${space}${units[unitIndex]}".format(size)                 // 整数显示
+        } + if (hide_bs) "" else bs
     }
 }
