187fe0753665beef49958a1c4e2a1bb61816f1b2
suqi8 <3383787570@qq.com>
2025-04-20 17:20:26 +0800

    feat(app): Remove ads in MMS app using DexKit
    -   Enabled ad removal in the MMS app via the `remove_message_ads` setting.
    -   Used DexKit to dynamically find and hook the target method.
    -   Hooked the found method to return an empty list, effectively removing ads.
    - Refactor code to use dexkit instead of hard code to find target class and method.

--- app/src/main/java/com/suqi8/oshin/hook/com/android/mms/mms.kt
+++ app/src/main/java/com/suqi8/oshin/hook/com/android/mms/mms.kt
@@ -3,21 +3,24 @@ package com.suqi8.oshin.hook.com.android.mms
 import android.annotation.SuppressLint
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
+import com.highcapable.yukihookapi.hook.log.YLog
+import org.luckypray.dexkit.DexKitBridge
 
 class mms: YukiBaseHooker() {
     @SuppressLint("UseCompatLoadingForDrawables", "SetTextI18n")
     override fun onHook() {
         loadApp("com.android.mms"){
             if (prefs("mms").getBoolean("remove_message_ads", false)) {
-                "oh.c".toClass().apply {
-                    method {
-                        name = "d"
-                        param("nh.j", "java.util.HashMap", "java.util.HashMap")
-                        returnType = "java.util.List"
-                    }.hook {
-                        before {
-                            result = emptyList<message_ads>()
+                DexKitBridge.create(this.appInfo.sourceDir).use {
+                    it.findMethod {
+                        matcher {
+                            paramTypes(null, "java.util.HashMap", "java.util.HashMap")
+                            returnType = "java.util.List"
+                            usingStrings("\\s")
                         }
+                    }.singleOrNull()?.also {
+                        YLog.info("methodName:"+it.methodName + " className:" + it.className)
+                        it.className.toClass().method { name = it.methodName }.hook { before { result = emptyList<message_ads>() } }
                     }
                 }
             }
