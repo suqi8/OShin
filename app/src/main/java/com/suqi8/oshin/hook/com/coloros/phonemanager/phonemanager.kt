2c2826f0a544d6d28c9b6efc17da7520096134ad
suqi8 <3383787570@qq.com>
2025-04-21 13:13:45 +0800

    feat(app): Refactor phone manager scan and prompt content hook with DexKit
    -   Refactored the hook for the phone manager scan circle view to use DexKit for method discovery.
    -   Updated the scan result method to use `DexKitBridge` to locate the target method dynamically.
    -   Implemented a method to modify scan prompt content by finding the method with specific modifiers and string content using DexKit.
    -   Optimized the code to use `singleOrNull()` to locate target method.
    -   Updated the logic for modifying the score and animation duration to use a dynamically found method.
    - Removed hard code class name to find target method.

--- app/src/main/java/com/suqi8/oshin/hook/com/coloros/phonemanager/phonemanager.kt
+++ app/src/main/java/com/suqi8/oshin/hook/com/coloros/phonemanager/phonemanager.kt
@@ -3,8 +3,6 @@ package com.suqi8.oshin.hook.com.coloros.phonemanager
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
 import com.highcapable.yukihookapi.hook.factory.method
 import com.highcapable.yukihookapi.hook.log.YLog
-import com.highcapable.yukihookapi.hook.type.java.BooleanType
-import com.highcapable.yukihookapi.hook.type.java.IntType
 import com.highcapable.yukihookapi.hook.type.java.UnitType
 import org.luckypray.dexkit.DexKitBridge
 import java.lang.reflect.Modifier
@@ -25,27 +23,6 @@ class phonemanager: YukiBaseHooker() {
                     }
                 }
             }
-            "com.oplus.phonemanager.common.view.ScanCircleView".toClass().apply {
-                method {
-                    name = "B"
-                    param(IntType, BooleanType, "java.lang.Long", BooleanType)
-                    returnType = UnitType
-                }.hook {
-                    before {
-                        if (prefs("phonemanager").getInt("custom_score", -1) != -1) {
-                            if (args[1] as Boolean) {
-                                hasani += 1
-                                if (hasani == 2) {
-                                    args[0] = prefs("phonemanager").getInt("custom_score", -1)
-                                }
-                            }
-                        }
-                        if (prefs("phonemanager").getInt("custom_animation_duration", -1) != -1) {
-                            args[2] = prefs("phonemanager").getInt("custom_animation_duration", -1).toLong()
-                        }
-                    }
-                }
-            }
             DexKitBridge.create(this.appInfo.sourceDir).use {
                 if (prefs("phonemanager").getString("custom_prompt_content", "") != "") {
                     it.findMethod {
@@ -54,11 +31,35 @@ class phonemanager: YukiBaseHooker() {
                             modifiers = Modifier.PUBLIC
                             usingStrings("updateScanResult manualOptItems: ","main_entry_summary")
                         }
-                    }.forEach {
+                    }.singleOrNull()?.also {
                         YLog.info("methodName:"+it.methodName + " className:" + it.className)
                         it.className.toClass().method { name = it.methodName }.hook {
                             before {
                                 result = prefs("phonemanager").getString("custom_prompt_content", "")
+                            }
+                        }
+                    }
+                }
+                it.findMethod {
+                    searchPackages("com.oplus.phonemanager.common.view")
+                    matcher {
+                        modifiers = Modifier.PUBLIC
+                        returnType = "void"
+                        usingStrings(" not change, skip","[setScore] ")
+                    }
+                }.singleOrNull()?.also {
+                    it.className.toClass().method { name = it.methodName }.hook {
+                        before {
+                            if (prefs("phonemanager").getInt("custom_score", -1) != -1) {
+                                if (args[1] as Boolean) {
+                                    hasani += 1
+                                    if (hasani == 2) {
+                                        args[0] = prefs("phonemanager").getInt("custom_score", -1)
+                                    }
+                                }
+                            }
+                            if (prefs("phonemanager").getInt("custom_animation_duration", -1) != -1) {
+                                args[2] = prefs("phonemanager").getInt("custom_animation_duration", -1).toLong()
                             }
                         }
                     }
