3fb1102c6fd846cdd0ae7a677f3a8adfc6f3ad33
suqi8 <3383787570@qq.com>
2025-04-21 12:38:08 +0800

    feat(app): Enhance hook logic with DexKit and add custom prompt content feature
    -   Refactored method hooking in `phonemanager` to use DexKit for dynamic method discovery.
    -   Added support for setting custom prompt content in `phonemanager` by hooking a method in the `com.oplus.phonemanager.newrequest.delegate` package.
    -   Enhanced `quicksearchbox` hook to use DexKit to locate specific methods related to removing ads.
    -   Added debug output in `quicksearchbox` to display found method name and class name for debug.
    - Added method for setting the limit of the number of automatic startup in battery setting.
    - Add comment for the code in `battery.kt` and `quicksearchbox.kt` to explain the usage of Dexkit.

--- app/src/main/java/com/suqi8/oshin/hook/com/heytap/quicksearchbox/quicksearchbox.kt
+++ app/src/main/java/com/suqi8/oshin/hook/com/heytap/quicksearchbox/quicksearchbox.kt
@@ -9,6 +9,17 @@ class quicksearchbox: YukiBaseHooker() {
     override fun onHook() {
         if (prefs("quicksearchbox").getBoolean("remove_app_recommendation_ads", false)) {
             loadApp(name = "com.heytap.quicksearchbox") {
+                /*DexKitBridge.create(this.appInfo.sourceDir).use {
+                    it.findMethod {
+                        searchPackages("com.heytap.quicksearchbox.ui.widget.advicesub")
+                        matcher {
+                            modifiers = Modifier.PUBLIC
+                            paramTypes(null, "java.lang.Boolean")
+                        }
+                    }.forEach {
+                        YLog.info("methodName:"+it.methodName + " className:" + it.className)
+                    }
+                }*/
                 "com.heytap.quicksearchbox.ui.widget.advicesub.AliveAppRecommendView".toClass().apply {
                     method {
                         name = "o"
