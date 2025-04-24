34441ddc0ee1ef663d1a63fe3ef10c86481a44ad
suqi8 <3383787570@qq.com>
2025-04-24 21:47:49 +0800

    feat(app): Update Recent Update page UI and logic
    -   Refactored the Recent Update page to use a `LazyColumn` for improved performance and scrolling behavior.
    -   Implemented haze effect for the `TopAppBar` with customizable blur settings.
    -   Added a back button to the `TopAppBar` for navigation.
    -   Updated the display of recent features using `SuperArrow` with dynamic route information.
    -   Added padding for display cutout and navigation bars to ensure proper layout on different devices.
    -   Added a spacer at the bottom of the `LazyColumn` to adjust for the navigation bar.
    -   Added an image to the top of the page.
    -   Added a new icon and update the logic for `TopAppBar`.
    -   Added some dependency and removed unused code.

--- app/src/main/java/com/suqi8/oshin/ui/activity/recent_update.kt
+++ app/src/main/java/com/suqi8/oshin/ui/activity/recent_update.kt
@@ -1,62 +1,148 @@
 package com.suqi8.oshin.ui.activity
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.highcapable.yukihookapi.hook.factory.prefs
 import com.suqi8.oshin.R
 import com.suqi8.oshin.features
 import com.suqi8.oshin.item
-import com.suqi8.oshin.ui.activity.funlistui.FunPage
 import com.suqi8.oshin.ui.activity.funlistui.addline
 import com.suqi8.oshin.utils.GetFuncRoute
+import dev.chrisbanes.haze.ExperimentalHazeApi
+import dev.chrisbanes.haze.HazeEffectScope
+import dev.chrisbanes.haze.HazeInputScale
+import dev.chrisbanes.haze.HazeProgressive
+import dev.chrisbanes.haze.HazeState
+import dev.chrisbanes.haze.HazeStyle
+import dev.chrisbanes.haze.HazeTint
+import dev.chrisbanes.haze.hazeEffect
+import dev.chrisbanes.haze.hazeSource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import top.yukonga.miuix.kmp.basic.Card
+import top.yukonga.miuix.kmp.basic.Icon
+import top.yukonga.miuix.kmp.basic.IconButton
+import top.yukonga.miuix.kmp.basic.MiuixScrollBehavior
+import top.yukonga.miuix.kmp.basic.Scaffold
+import top.yukonga.miuix.kmp.basic.TopAppBar
+import top.yukonga.miuix.kmp.basic.rememberTopAppBarState
 import top.yukonga.miuix.kmp.extra.SuperArrow
+import top.yukonga.miuix.kmp.icon.MiuixIcons
+import top.yukonga.miuix.kmp.icon.icons.useful.Back
+import top.yukonga.miuix.kmp.theme.MiuixTheme
+import top.yukonga.miuix.kmp.utils.overScrollVertical
 
+@OptIn(ExperimentalHazeApi::class)
 @Composable
 fun recent_update(navController: NavController) {
-    FunPage(
-        title = stringResource(R.string.recent_update),
-        navController = navController
-    ) {
-        Card(
-            modifier = Modifier.fillMaxWidth()
-                .padding(horizontal = 12.dp)
-                .padding(bottom = 6.dp,top = 15.dp)
-        ) {
-            val recentFeatureState = remember { mutableStateOf<List<item>?>(null) }
-            val context = LocalContext.current
-            LaunchedEffect(Unit) {
-                recentFeatureState.value = withContext(Dispatchers.IO) {
-                    features(context)
-                        .takeIf { it.isNotEmpty() }
-                        ?.toList()
-                        ?.reversed()
+    val context = LocalContext.current
+    val topAppBarState = MiuixScrollBehavior(rememberTopAppBarState())
+    val alpha = context.prefs("settings").getFloat("AppAlpha", 0.75f)
+    val blurRadius: Dp = context.prefs("settings").getInt("AppblurRadius", 25).dp
+    val noiseFactor = context.prefs("settings").getFloat("AppnoiseFactor", 0f)
+    val containerColor: Color = MiuixTheme.colorScheme.background
+    val hazeState = remember { HazeState() }
+    val hazeStyle = remember(containerColor, alpha, blurRadius, noiseFactor) {
+        HazeStyle(
+            backgroundColor = containerColor,
+            tint = HazeTint(containerColor.copy(alpha)),
+            blurRadius = blurRadius,
+            noiseFactor = noiseFactor
+        )
+    }
+    val lazyListState = rememberSaveable(saver = LazyListState.Saver) {
+        LazyListState(firstVisibleItemIndex = 0) // 初始位置
+    }
+    Scaffold(topBar = {
+        TopAppBar(
+            scrollBehavior = topAppBarState,
+            title = stringResource(R.string.recent_update),
+            color = if (context.prefs("settings").getBoolean("enable_blur", true)) Color.Transparent else MiuixTheme.colorScheme.background,
+            modifier = if (context.prefs("settings").getBoolean("enable_blur", true)) {
+                Modifier.hazeEffect(
+                    state = hazeState,
+                    style = hazeStyle, block = fun HazeEffectScope.() {
+                        inputScale = HazeInputScale.Auto
+                        if (context.prefs("settings").getBoolean("enable_gradient_blur", true)) progressive = HazeProgressive.verticalGradient(startIntensity = 1f, endIntensity = 0f)
+                    })
+            } else Modifier,
+            navigationIcon = {
+                IconButton(
+                    onClick = {
+                        navController.popBackStack()
+                    },
+                    modifier = Modifier.padding(start = 18.dp)
+                ) {
+                    Icon(
+                        imageVector = MiuixIcons.Useful.Back,
+                        contentDescription = null,
+                        tint = MiuixTheme.colorScheme.onBackground
+                    )
                 }
             }
-            recentFeatureState.value?.forEachIndexed() { index, feature ->
-                key(feature.title) {  // 使用唯一标识作为 key
+        )
+        Image(painter = painterResource(R.drawable.osu),contentDescription = null, modifier = Modifier.fillMaxWidth())
+    }) { padding ->
+        val recentFeatureState = remember { mutableStateOf<List<item>?>(null) }
+        val context = LocalContext.current
+        LaunchedEffect(Unit) {
+            recentFeatureState.value = withContext(Dispatchers.IO) {
+                features(context)
+                    .takeIf { it.isNotEmpty() }
+                    ?.toList()
+                    ?.reversed()
+            }
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .overScrollVertical()
+                .hazeSource(state = hazeState)
+                .background(MiuixTheme.colorScheme.background)
+                .windowInsetsPadding(WindowInsets.displayCutout.only(WindowInsetsSides.Horizontal))
+                .windowInsetsPadding(WindowInsets.navigationBars.only(WindowInsetsSides.Horizontal))
+                .nestedScroll(topAppBarState.nestedScrollConnection),
+            contentPadding = padding,
+            state = lazyListState
+        ) {
+            recentFeatureState.value?.let { list ->
+                itemsIndexed(list) { index, feature ->
                     val onClick by remember(feature.category) {
-                        mutableStateOf({
-                            navController.navigate(feature.category)
-                        })
+                        mutableStateOf({ navController.navigate(feature.category) })
                     }
                     val route = rememberSaveable { mutableStateOf("") }
                     if (route.value == "") {
                         LaunchedEffect(Unit) {
-                            route.value = GetFuncRoute(feature.category,context)
+                            route.value = GetFuncRoute(feature.category, context)
                         }
                     }
                     SuperArrow(
@@ -64,10 +150,13 @@ fun recent_update(navController: NavController) {
                         summary = if (feature.summary != null) feature.summary + "\n" + route.value else route.value,
                         onClick = onClick
                     )
-                    if (index < recentFeatureState.value!!.size - 1) {
+                    if (index != list.lastIndex) {
                         addline()
                     }
                 }
+            }
+            item {
+                Spacer(Modifier.height(WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()))
             }
         }
     }
