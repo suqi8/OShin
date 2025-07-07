2fa1819909b5ee41cda411c710e0a6b28ca8a709
suqi8 <3383787570@qq.com>
2025-07-07 19:45:30 +0800

    Refactor(SystemUI): Update Notification hook with KavaRef
    This commit updates the notification hook for `com.android.systemui` to use KavaRef for method resolution.
    
    Specifically, in `app/src/main/java/com/suqi8/oshin/hook/com/android/systemui/StatusBar/Notification.kt`, the method finding logic for `updateDeveloperMode` within `com.oplus.systemui.statusbar.controller.SystemPromptController` has been changed from using `method {}` to `resolve().firstMethod {}`.
    
    This change leverages KavaRef's capabilities for more robust method identification.

--- app/src/main/java/com/suqi8/oshin/hook/com/android/systemui/StatusBar/Notification.kt
+++ app/src/main/java/com/suqi8/oshin/hook/com/android/systemui/StatusBar/Notification.kt
@@ -3,14 +3,13 @@ package com.suqi8.oshin.hook.com.android.systemui.StatusBar
 import com.highcapable.kavaref.KavaRef.Companion.resolve
 import com.highcapable.kavaref.condition.type.Modifiers
 import com.highcapable.yukihookapi.hook.entity.YukiBaseHooker
-import com.highcapable.yukihookapi.hook.factory.method
 
 class Notification: YukiBaseHooker() {
     override fun onHook() {
         if (prefs("systemui\\notification").getBoolean("remove_developer_options_notification", false)) {
             loadApp(name = "com.android.systemui") {
-                "com.oplus.systemui.statusbar.controller.SystemPromptController".toClass().apply {
-                    method {
+                "com.oplus.systemui.statusbar.controller.SystemPromptController".toClass().resolve().apply {
+                    firstMethod {
                         name = "updateDeveloperMode"
                     }.hook {
                         replaceUnit {
